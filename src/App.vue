0cdab13857f4bb67b66cd22db63a85cf04345c5c
katya-sy <katya_sy_28@mail.ru>
2023-12-27 17:47:12 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div class="post" v-for="post in posts">
+      <div><strong>Title:</strong> {{ post.title }}</div>
+      <div><strong>Desc:</strong> {{ post.body }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      posts: [
+        { id: 1, title: "Jsjsjs", body: "Js js js js js js" },
+        { id: 2, title: "Ararar", body: "Js js js js js js" },
+        { id: 3, title: "Upupu", body: "Js js js js js js" },
+      ],
+    };
+  },
+
+  methods: {},
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.post {
+  margin-top: 10px;
+  padding: 15px;
+  border: 3px solid aqua;
+}
+</style>
