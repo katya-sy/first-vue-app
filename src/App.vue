a79b628fbc607f52d180287ffb9230a178a0247a
katya-sy <katya_sy_28@mail.ru>
2023-12-27 18:18:27 +0900

    add creating posts

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,23 @@
 <template>
-  <div>
+  <div class="app">
+    <form @submit.prevent>
+      <h4>Creating new post</h4>
+      <input
+        v-bind:value="title"
+        @input="title = $event.target.value"
+        class="input"
+        type="text"
+        placeholder="Title"
+      />
+      <input
+        v-bind:value="body"
+        @input="body = $event.target.value"
+        class="input"
+        type="text"
+        placeholder="Desc"
+      />
+      <button class="btn" @click="createPost">Create</button>
+    </form>
     <div class="post" v-for="post in posts">
       <div><strong>Title:</strong> {{ post.title }}</div>
       <div><strong>Desc:</strong> {{ post.body }}</div>
@@ -16,10 +34,23 @@ export default {
         { id: 2, title: "Ararar", body: "Js js js js js js" },
         { id: 3, title: "Upupu", body: "Js js js js js js" },
       ],
+      title: "",
+      body: "",
     };
   },
 
-  methods: {},
+  methods: {
+    createPost() {
+      const newPost = {
+        id: Date.now(),
+        title: this.title,
+        body: this.body,
+      };
+      this.posts.push(newPost);
+      this.title = "";
+      this.body = "";
+    },
+  },
 };
 </script>
 
@@ -29,10 +60,32 @@ export default {
   padding: 0;
   box-sizing: border-box;
 }
-
+.app {
+  margin: 0 auto;
+  max-width: 1000px;
+  padding: 20px;
+}
+form {
+  display: flex;
+  flex-direction: column;
+}
 .post {
   margin-top: 10px;
   padding: 15px;
-  border: 3px solid aqua;
+  border: 2px solid aqua;
+}
+.input {
+  width: 100%;
+  margin-top: 10px;
+  padding: 10px 15px;
+  border: 1px solid aqua;
+}
+.btn {
+  margin-top: 10px;
+  padding: 10px 15px;
+  background: none;
+  border: 1px solid aqua;
+  color: #000;
+  align-self: flex-end;
 }
 </style>
