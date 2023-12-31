4cb19094cc3be0abad2a87aa8577e3c36e8eeb50
katya-sy <katya_sy_28@mail.ru>
2023-12-31 10:11:41 +0900

    deleting posts

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
     <h3>Post list</h3>
-    <post-item v-for="post in posts" :post="post"></post-item>
+    <post-item
+      v-for="post in posts"
+      :post="post"
+      :key="post.id"
+      @remove="$emit('remove', post)"
+    ></post-item>
   </div>
 </template>
 
