56ac5a2bce46ce0b192b7c03d0fafcd31787abf5
katya-sy <katya_sy_28@mail.ru>
2024-01-01 14:37:39 +0900

    add infinite scroll

--- src/App.vue
+++ src/App.vue
@@ -15,11 +15,12 @@
       v-if="!isPostLoading"
     />
     <div v-else>Loading...</div>
-    <my-pagination
+    <div ref="observer" class="observer"></div>
+    <!-- <my-pagination
       @changePage="changePage"
       :page="page"
       :pages="totalPages"
-    ></my-pagination>
+    ></my-pagination> -->
   </div>
 </template>
 
@@ -85,12 +86,46 @@ export default {
         this.isPostLoading = false;
       }
     },
-    changePage(pageNumber) {
-      this.page = pageNumber;
+    async loadMorePosts() {
+      try {
+        this.page += 1;
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/posts",
+          {
+            params: {
+              _page: this.page,
+              _limit: this.limit,
+            },
+          }
+        );
+        this.totalPages = Math.ceil(
+          response.headers["x-total-count"] / this.limit
+        );
+        this.posts = [...this.posts, ...response.data];
+      } catch (error) {
+        alert(`Error: ${error}`);
+      }
     },
+    // changePage(pageNumber) {
+    //   this.page = pageNumber;
+    // },
   },
   mounted() {
     this.fetchPosts();
+
+    let options = {
+      rootMargin: "0px",
+      threshold: 1.0,
+    };
+
+    const callback = (entries, observer) => {
+      if (entries[0].isIntersecting && this.page < this.totalPages) {
+        this.loadMorePosts();
+      }
+    };
+
+    let observer = new IntersectionObserver(callback, options);
+    observer.observe(this.$refs.observer);
   },
   computed: {
     sortedPosts() {
@@ -105,9 +140,9 @@ export default {
     },
   },
   watch: {
-    page() {
-      this.fetchPosts();
-    },
+    // page() {
+    //   this.fetchPosts();
+    // },
   },
 };
 </script>
